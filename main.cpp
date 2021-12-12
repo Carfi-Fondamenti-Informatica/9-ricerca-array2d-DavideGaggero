9ff8c7c99cbacfe49ce7cc15ac8cdae80b11be06
DavideGaggero <91790589+DavideGaggero@users.noreply.github.com>
2021-12-12 02:34:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include "lib.h"
 using namespace std;
 
-int main(){
+int main() {
   int pos;
   char a[10][20], f[1][20];
   for (int i=0; i<10; i++){
-    cin >> a{i}
+    cin >> a{i};
   }
   cin >> f[0];
   
